61ca6d2bb82e73abe7b1a239511c9a89534e5a8b
Dimitrius <shimansky.dmitry@gmail.com>
2024-09-24 22:11:15 +0300

    add: test case plan create

---
+++ src/tests/test.spec.ts
@@ -0,0 +1,15 @@
+import {test, expect} from "../pages/pageObjectManager/pageObjectManager";
+
+test.describe("Cart", async () => {
+    test.beforeEach(async (done) => {});
+
+    test("Move to empty cart", async () => {});
+
+    test("Move to cart with 1 not promotion product", async () => {});
+
+    test("Move to cart with 1 promotion product", async () => {});
+
+    test("Move to cart with 9 different products", async () => {});
+
+    test("Move to cart with 9 promotion products with the same product name", async () => {});
+});
